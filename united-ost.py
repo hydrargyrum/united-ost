116f0fcce66f7d3f0896f18c7cb519cd0db9caeb
Hg <dev@indigo.re>
2020-10-24 11:55:02 +0200

    support multiple assets files + different dir

--- united-ost.py
+++ united-ost.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python3
 
 import logging
+import os
+from pathlib import Path
 import sys
 
-import UnityPy
-import UnityPy.Logger
+try:
+    import UnityPy
+except ImportError:
+    raise ImportError('Please pip install UnityPy')
 
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO)
 #logging.root.handlers[0].setFormatter(UnityPy.Logger.ColoredFormatter())
 #logging.root.addHandler(UnityPy.Logger.ListHandler())
 
+orig_path = Path.cwd()
 
-am = UnityPy.AssetsManager()
-am.load_file(sys.argv[1])
 
-sf, = am.assets.values()
-# msk = [v for v in sf.objects.values() if isinstance(v.read(), UnityPy.classes.AudioClip)]
-msk = [v for v in sf.objects.values() if v.type == 'AudioClip']
-for m in msk:
-    obj = m.read()
-    # with open(f'{mm.name}.dat', 'wb') as fp:
-    #     fp.write(mm.m_AudioData)
-    import code; code.interact(local=locals())
-    # print(obj.m_AudioData)
-    for filename, mem in obj.samples.items():
-        try:
-            with open(filename, 'xb') as fp:
-                fp.write(mem)
-        except FileExistsError:
-            print(f"Will not overwrite exisisting file {filename}")
-        else:
-            print(f"Wrote {filename}")
+for asset_fn in sys.argv[1:]:
+    os.chdir(orig_path)
+    # UnityPy searches for .resource files relative to current directory
+    # not relative to .assets dir...
+    os.chdir(Path(asset_fn).parent)
+
+    am = UnityPy.AssetsManager()
+    am.load_file(asset_fn)
+
+    sf, = am.assets.values()
+    # msk = [v for v in sf.objects.values() if isinstance(v.read(), UnityPy.classes.AudioClip)]
+    msk = [v for v in sf.objects.values() if v.type == 'AudioClip']
+    for m in msk:
+        obj = m.read()
+        #~ with open(f'{obj.name}.dat', 'wb') as fp:
+            #~ fp.write(obj.m_AudioData)
+        #~ import code; code.interact(local=locals())
+        # print(obj.m_AudioData)
+
+        for filename, mem in obj.samples.items():
+            target = orig_path / filename
+            try:
+                with open(target, 'xb') as fp:
+                    fp.write(mem)
+            except FileExistsError:
+                print(f"Will not overwrite exisisting file {filename}")
+            else:
+                print(f"Wrote {filename}")
